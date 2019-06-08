bec26e4bc4826aeb4dedb8c4165da6e174e7a197
moezboukhari <boukhari.moez@gmail.com>
2019-06-08 11:57:28 +0200

    init

--- src/main/java/org/sid/web/EventReactiveRestAPI.java
+++ src/main/java/org/sid/web/EventReactiveRestAPI.java
@@ -7,7 +7,6 @@ import java.util.stream.Stream;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
